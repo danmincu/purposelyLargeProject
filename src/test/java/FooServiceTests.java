12227f58a5c4c776992e7d0f7dfb88004e5dab19
Dan Mincu <dan.mincu@jsitelecom.com>
2018-12-06 16:06:52 -0500

    first commit

---
+++ src/test/java/FooServiceTests.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FooServiceTests {
+
+    @Test
+    public void testFooMethod() {
+        FooService subject = new FooService(); // MyClass is tested
+
+        // assert statements
+        assertEquals(11, subject.addFoos(10, 1));
+    }
+
+    @Test
+    public void testFooFooMethod() {
+        FooService subject = new FooService(); // MyClass is tested
+
+        // assert statements
+        assertEquals(30, subject.addFooFoos(1, 2));
+    }
+
+    @Test
+    public void testFooIntermMethod() {
+        FooService subject = new FooService(); // MyClass is tested
+
+        // assert statements
+        assertEquals(3, subject.addFooFoos(1, 2));
+    }
+
+}
